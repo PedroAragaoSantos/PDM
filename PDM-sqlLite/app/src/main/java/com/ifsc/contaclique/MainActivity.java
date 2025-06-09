7bdec9636bb0da8f403d4cb9d47db4e5cb97e7b6
Pedro <ph1953785@gmail.com>
2025-06-09 07:46:49 -0300

    SQlite.2

--- PDM-sqlLite/app/src/main/java/com/ifsc/contaclique/MainActivity.java
+++ PDM-sqlLite/app/src/main/java/com/ifsc/contaclique/MainActivity.java
@@ -3,6 +3,7 @@ package com.ifsc.contaclique;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabaseLockedException;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -20,51 +21,44 @@ public class MainActivity extends AppCompatActivity {
 
     int i=0;
     SQLiteDatabase db;
-    Button buttonInsere;
+
     EditText editText;
+    Button botaoInserir;
 
     ListView listView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        db =openOrCreateDatabase("banco",MODE_PRIVATE, null);
 
-        db = openOrCreateDatabase("banco",MODE_PRIVATE,null);
-
-        db.execSQL("CREATE TABLE IF NOT EXISTS notas (id INTEGER PRIMARY KEY AUTOINCREMENT, txt VARCHAR)");
-        buttonInsere = findViewById(R.id.button);
+        db.execSQL("CREATE TABLE IF NOT EXISTS notas(id INTEGER PRIMARY KEY AUTOINCREMENT, txt TEXT)");
         editText = findViewById(R.id.editTextText);
-        listView=findViewById(R.id.listView);
-        buttonInsere.setOnClickListener(v->{
+        botaoInserir = findViewById(R.id.button);
+        listView = findViewById(R.id.lista);
+        botaoInserir.setOnClickListener(v-> {
             String msg = editText.getText().toString();
-            listagemNotas();
-            insereNota(msg);
+            listagem();
+            inserir(msg);
+
         });
     }
-
-    public void listagemNotas(){
+    public void listagem(){
         Cursor cursor = db.rawQuery("SELECT * FROM notas",null);
         cursor.moveToFirst();
-        ArrayList<String> ListaNotas  = new ArrayList<String>();
-        while
-        (!cursor.isAfterLast()){
+        ArrayList<String> listaTexto = new ArrayList<String>();
+        while (!cursor.isAfterLast()){
             int coluna = cursor.getColumnIndex("txt");
-            ListaNotas.add(cursor.getString(coluna));
+            listaTexto.add(cursor.getString(coluna));
             cursor.moveToNext();
         }
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,
-                android.R.id.text1,
-                ListaNotas
-        );
-        listView.setAdapter(adapter);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, listaTexto);
     }
-    public void insereNota(String txt){
-        db.execSQL("INSERT INTO notas (txt) VALUES('"+txt+"'); ");
+    public void inserir(String txt){
         ContentValues cv = new ContentValues();
         cv.put("txt",txt);
-        db.insert("notas",null,cv);
+        db.insert("notas",null, cv);
+
     }
 }
